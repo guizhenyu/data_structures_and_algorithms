a6bb2a37a8621fdd79cb55e60f0eb9ed56c58af3
guizhenyu <guizy@deepblueai.com>
2021-12-21 10:55:48 +0800

    add BubbleSort

--- src/main/java/com/gzy/code/basic/class01/BubbleSort.java
+++ src/main/java/com/gzy/code/basic/class01/BubbleSort.java
@@ -1,10 +1,130 @@
 package com.gzy.code.basic.class01;
 
+import static com.gzy.code.basic.class01.SelectionSort.printArray;
+
+import java.util.Arrays;
+
 /**
  * description: BubbleSort date: 2021/12/20 3:27 下午
  *
  * @author: guizhenyu
  */
 public class BubbleSort {
+
+  public static void bubbleSort(int[] arr){
+    if (arr == null || arr.length == 0){
+      return;
+    }
+
+
+    for (int i = 0; i < arr.length - 1; i ++){
+      for (int j = i + 1; j < arr.length; j ++){
+        if (arr[i] > arr[j]){
+          swap(arr, i, j);
+        }
+      }
+    }
+  }
+
+
+  public static void swap(int[] arr, int i, int j){
+    arr[i] = arr[i] ^ arr[j];
+    arr[j] = arr[i] ^ arr[j];
+    arr[i] = arr[i] ^ arr[j];
+  }
+
+
+  public static void comparator(int[] arr){
+    Arrays.sort(arr);
+  }
+
+  public static void main(String[] args) {
+    /**
+     * 验证我们写的算法排序正确性
+     * 1. 最简单的方法实现比较功能（不考虑时间复杂度和空间复杂度）
+     * 2. 然后随机生成数组，通过两种排序方法验证，自己写的算法正确性
+     * 3. 通过大批量次数去验证
+     *
+     */
+
+    int testTime = 500000;
+    int maxSize = 100;
+    int maxValue = 100;
+
+
+    boolean succeed = true;
+    for (int i = 0; i < testTime; i++){
+
+      int[] arr = generateArrayRandom(maxSize, maxValue);
+      int[] arr1 = copyArray(arr);
+
+      bubbleSort(arr);
+      comparator(arr1);
+
+      if(!equals(arr, arr1)){
+        succeed = false;
+        printArray(arr);
+        printArray(arr1);
+        break;
+      }
+    }
+
+    System.out.println(succeed?"Nice!":"Fucking fucked!");
+
+
+  }
+
+  private static boolean equals(int[] arr, int[] arr1) {
+
+    if ((arr == null && arr1 != null) || (arr != null && arr1 == null)){
+      return false;
+    }
+
+    if (arr == null && arr1 == null){
+      return true;
+    }
+
+    if(arr.length != arr.length){
+      return false;
+    }
+
+
+    for (int i = 0; i < arr.length; i ++){
+
+      if(arr[i] != arr1[i]){
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  private static int[] copyArray(int[] arr) {
+    if (null == arr){
+      return null;
+    }
+
+    int[] newArray = new int[arr.length];
+
+    for (int i = 0; i < arr.length; i++){
+      newArray[i] = arr[i];
+    }
+    return newArray;
+  }
+
+  private static int[] generateArrayRandom(int maxSize, int maxValue) {
+
+
+    int arrSize = (int)((maxSize + 1) * Math.random());
+
+    int[] arr = new int[arrSize];
+
+    for (int i = 0; i < arrSize; i++){
+      arr[i] = (int)((maxValue + 1) * Math.random()) - (int)((maxValue) * Math.random());
+    }
+
+    return arr;
+
+  }
 
 }
--- src/main/java/com/gzy/code/basic/class01/Solution.java
+++ src/main/java/com/gzy/code/basic/class01/Solution.java
@@ -1,93 +1,93 @@
-package com.gzy.code.basic.class01;
-
-//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+//package com.gzy.code.basic.class01;
+//
+////给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+////
+////
+////
+//// 示例 1：
+////
+////
+////输入：head = [1,2,3,4,5], n = 2
+////输出：[1,2,3,5]
+////
+////
+//// 示例 2：
+////
+////
+////输入：head = [1], n = 1
+////输出：[]
+////
+////
+//// 示例 3：
+////
+////
+////输入：head = [1,2], n = 1
+////输出：[1]
+////
+////
+////
+////
+//// 提示：
+////
+////
+//// 链表中结点的数目为 sz
+//// 1 <= sz <= 30
+//// 0 <= Node.val <= 100
+//// 1 <= n <= sz
+////
+////
+////
+////
+//// 进阶：你能尝试使用一趟扫描实现吗？
+//// Related Topics 链表 双指针
+//// 👍 1715 👎 0
 //
 //
+////leetcode submit region begin(Prohibit modification and deletion)
+///**
+// * Definition for singly-linked list.
+// * public class ListNode {
+// *     int val;
+// *     ListNode next;
+// *     ListNode() {}
+// *     ListNode(int val) { this.val = val; }
+// *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+// * }
+// */
+//class Solution {
+//    public ListNode removeNthFromEnd(ListNode head, int n) {
 //
-// 示例 1：
+//        int size = 0;
+//
+//        ListNode pre = null;
+//        ListNode next = null;
+//
+//        while (null != head.next){
+//
+//            if (size == n + 1){
+//                pre = head;
+//                next = head.next.next;
+//            }
+//
+//            if (size > n + 1){
+//                pre = pre.next;
+//                next = next.next;
+//            }
+//            size ++;
+//        }
+//
+//        if (size > n){
+//            pre.next = next;
+//            return head;
+//        }
+//
+//        if (size == n){
+//            return head.next;
+//        }
+//
+//        return head;
 //
 //
-//输入：head = [1,2,3,4,5], n = 2
-//输出：[1,2,3,5]
-//
-//
-// 示例 2：
-//
-//
-//输入：head = [1], n = 1
-//输出：[]
-//
-//
-// 示例 3：
-//
-//
-//输入：head = [1,2], n = 1
-//输出：[1]
-//
-//
-//
-//
-// 提示：
-//
-//
-// 链表中结点的数目为 sz
-// 1 <= sz <= 30
-// 0 <= Node.val <= 100
-// 1 <= n <= sz
-//
-//
-//
-//
-// 进阶：你能尝试使用一趟扫描实现吗？
-// Related Topics 链表 双指针
-// 👍 1715 👎 0
-
-
-//leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-
-        int size = 0;
-
-        ListNode pre = null;
-        ListNode next = null;
-
-        while (null != head.next){
-
-            if (size == n + 1){
-                pre = head;
-                next = head.next.next;
-            }
-
-            if (size > n + 1){
-                pre = pre.next;
-                next = next.next;
-            }
-            size ++;
-        }
-
-        if (size > n){
-            pre.next = next;
-            return head;
-        }
-
-        if (size == n){
-            return head.next;
-        }
-
-        return head;
-
-
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+//    }
+//}
+////leetcode submit region end(Prohibit modification and deletion)
