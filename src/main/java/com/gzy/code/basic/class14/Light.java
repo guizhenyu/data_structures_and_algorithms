a8a5ba35868307dccbbd2d45ee7ac6c86021e7fd
guizhenyu <guizy@deepblueai.com>
2022-06-28 18:27:41 +0800

    add light

---
+++ src/main/java/com/gzy/code/basic/class14/Light.java
@@ -0,0 +1,107 @@
+package com.gzy.code.basic.class14;
+
+import java.util.HashSet;
+
+/**
+ * description: Light date: 2022/6/28 17:39
+ *
+ * @author: guizhenyu
+ */
+public class Light {
+
+  /**
+   * 给定一个字符串str，只由‘X’和‘.’两种字符构成。
+   * ‘X’表示墙，不能放灯，也不需要点亮
+   * ‘.’表示居民点，可以放灯，需要点亮
+   * 如果灯放在i位置，可以让i-1，i和i+1三个位置被点亮
+   * 返回如果点亮str中所有需要点亮的位置，至少需要几盏灯
+   *
+   */
+
+
+  public static void main(String[] args) {
+    int len = 20;
+    int testTime = 100000;
+    for (int i = 0; i < testTime; i++) {
+      String test = randomString(len);
+      int ans1 = minLight1(test);
+      int ans2 = minLight2(test);
+      if (ans1 != ans2) {
+        System.out.println("oops!");
+      }
+    }
+    System.out.println("finish!");
+  }
+
+  private static int minLight2(String test) {
+    /**
+     * 思路：
+     * 谈薪算法
+     *
+     *
+     *
+     */
+
+
+  }
+
+  private static int minLight1(String road) {
+
+    if (road == null || road.length() == 0) {
+      return 0;
+    }
+    return process(road.toCharArray(), 0, new HashSet<>());
+  }
+
+  /**
+   * 暴力递归
+   * 到最后一位时，把所以的排序遍历一下，如果发现连续3个位置点是'.' 的地方都没有灯的情况，就证明永远都点亮不了，
+   * 否则直接返回当前HashSet的size
+   *
+   * @param arr
+   * @param index
+   * @param lights
+   * @return
+   */
+  private static int process(char[] arr, int index, HashSet<Integer> lights) {
+    if (index == arr.length){
+      for (int i = 1; i < index - 1; i++){
+        if (arr[i] != 'X'){
+          if (!lights.contains(i - 1) && !lights.contains(i) && !lights.contains(i + 1)){
+            return Integer.MAX_VALUE;
+          }
+        }
+
+      }
+
+      return lights.size();
+    }else {
+
+      int no = process(arr, index + 1, lights);
+      int yes = Integer.MAX_VALUE;
+      if (arr[index] == '.'){
+        lights.add(index);
+        yes = process(arr, index + 1, lights);
+        // 恢复现场
+        lights.remove(index);
+      }
+
+      return Math.min(no, yes);
+    }
+
+  }
+
+  // for test
+  public static String randomString(int len) {
+    char[] res = new char[(int) (Math.random() * len) + 1];
+    for (int i = 0; i < res.length; i++) {
+      res[i] = Math.random() < 0.5 ? 'X' : '.';
+    }
+    return String.valueOf(res);
+  }
+
+}
+
+
+
+}
