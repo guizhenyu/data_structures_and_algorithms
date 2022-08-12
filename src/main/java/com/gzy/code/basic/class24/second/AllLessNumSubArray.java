1b3e0628c4dff07210621615f9eb013fdbe63909
guizhenyu <guizy@deepblueai.com>
2022-08-12 18:30:49 +0800

    add 双端队列

---
+++ src/main/java/com/gzy/code/basic/class24/second/AllLessNumSubArray.java
@@ -0,0 +1,15 @@
+package com.gzy.code.basic.class24.second;
+
+/**
+ * description: AllLessNumSubArray date: 2022/8/11 17:28
+ *
+ * 给定一个整型数组arr，和一个整数num
+ * 某个arr中的子数组sub，如果想达标，必须满足：sub中最大值 – sub中最小值 <= num，
+ * 返回arr中达标子数组的数量
+ *
+ *
+ * @author: guizhenyu
+ */
+public class AllLessNumSubArray {
+
+}
