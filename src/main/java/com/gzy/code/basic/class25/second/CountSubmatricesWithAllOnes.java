0f1eb67a7321a7f7c3a1c4d13d1b4ce71af8aa20
guizhenyu <guizy@deepblueai.com>
2022-08-26 18:05:57 +0800

    add CountSubmatricesWithAllOnes

---
+++ src/main/java/com/gzy/code/basic/class25/second/CountSubmatricesWithAllOnes.java
@@ -0,0 +1,17 @@
+package com.gzy.code.basic.class25.second;
+
+/**
+ * description: CountSubmatricesWithAllOnes date: 2022/8/26 15:10
+ *
+ * @author: guizhenyu
+ */
+public class CountSubmatricesWithAllOnes {
+
+  public static int numSubmat(int[][] mat) {
+
+
+
+
+  }
+
+}
